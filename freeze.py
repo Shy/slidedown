8fa02605f475669a8954d10ce62716f310009612
Shy Ruparel <Shyamalruparel1991@gmail.com>
2019-05-23 16:31:45 +0200

    Added reveal js and messed around with SSG

--- freeze.py
+++ freeze.py
@@ -2,6 +2,7 @@ from flask_frozen import Freezer
 from app import app
 import contentful
 import os
+import shutil
 
 SPACE_ID = os.environ.get("SPACE_ID")
 DELIVERY_API_KEY = os.environ.get("DELIVERY_API_KEY")
@@ -17,6 +18,35 @@ def deck_by_id():
     for deck in decks:
         yield {"deck_id": deck.id}
 
+def copytree(src, dst, symlinks = False, ignore = None):
+  if not os.path.exists(dst):
+    os.makedirs(dst)
+    shutil.copystat(src, dst)
+  lst = os.listdir(src)
+  if ignore:
+    excl = ignore(src, lst)
+    lst = [x for x in lst if x not in excl]
+  for item in lst:
+    s = os.path.join(src, item)
+    d = os.path.join(dst, item)
+    if symlinks and os.path.islink(s):
+      if os.path.lexists(d):
+        os.remove(d)
+      os.symlink(os.readlink(s), d)
+      try:
+        st = os.lstat(s)
+        mode = stat.S_IMODE(st.st_mode)
+        os.lchmod(d, mode)
+      except:
+        pass # lchmod not available
+    elif os.path.isdir(s):
+      copytree(s, d, symlinks, ignore)
+    else:
+      shutil.copy2(s, d)
 
 if __name__ == "__main__":
     freezer.freeze()
+    source = "reveal.js"
+    destination = "build"
+    # create a backup directory
+    copytree(source, destination)
---
+++ reveal.js/generate.md
@@ -0,0 +1,22 @@
+# Yo Toronto!!!
+## What's up Shy?
+Let's have some maple syrup
+
+
+
+# Berlin 1
+![GOODNIGHT](//images.ctfassets.net/jokk403r56yp/7zP2ZK3l2cDPNpSukAxG9E/c94049bd7e2a805a4c470ee1257eb4de/GOODNIGHT.png)
+
+
+
+# Berlin 2
+## Check check
+Yo
+Yo
+
+
+
+# Berlin 3
+1. Yo
+2. Yo
+3. YoYo
